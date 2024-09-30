02f930aa1047e8ffd35df1bacd9d317e63c21485
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-09-30 14:03:25 -0700

    SprightBlight V0.01
    Additional formatting fixed, for the sake of my sanity. Better comments so that I can modify this more efficiently.
    
    Added a header of my own copyright, since we will be majorly differing from this skeleton structure.

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -1,36 +1,31 @@
-/*******************************************************************************************
+/************************************************************************
+*   <Spright Blight>
+*   <Bullet hell spright game>
 *
-*   raylib game template
+*   This game was created via raylib,
+*   Credit to raysan5 for designing this incredible engine
 *
-*   <Game title>
-*   <Game description>
-*
-*   This game has been created using raylib (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+*   Copyright (c) 2024 - Ryan Wiseman
+************************************************************************/
 
 #include "raylib.h"
-#include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
+// NOTE: Declares global (extern) variables and screens functions
+#include "screens.h"    
 
+/*We include some web headers so that
+this game translates to the internet.*/
 #if defined(PLATFORM_WEB)
     #include <emscripten/emscripten.h>
 #endif
 
-//----------------------------------------------------------------------------------
 // Shared Variables Definition (global)
 // NOTE: Those variables are shared between modules through screens.h
-//----------------------------------------------------------------------------------
 GameScreen currentScreen = LOGO;
 Font font = { 0 };
 Music music = { 0 };
 Sound fxCoin = { 0 };
 
-//----------------------------------------------------------------------------------
 // Local Variables Definition (local to this module)
-//----------------------------------------------------------------------------------
 static const int screenWidth = 800;
 static const int screenHeight = 450;
 
@@ -41,30 +36,37 @@ static bool transFadeOut = false;
 static int transFromScreen = -1;
 static GameScreen transToScreen = UNKNOWN;
 
-//----------------------------------------------------------------------------------
-// Local Functions Declaration
-//----------------------------------------------------------------------------------
-static void ChangeToScreen(int screen);     // Change to screen, no transition effect
+/*Local Functions Declarations are here:*/
 
-static void TransitionToScreen(int screen); // Request transition to next screen
-static void UpdateTransition(void);         // Update transition effect
-static void DrawTransition(void);           // Draw transition effect (full-screen rectangle)
+// Change to screen, no transition effect
+static void ChangeToScreen(int screen);     
+// Request transition to next screen
+static void TransitionToScreen(int screen); 
+// Update transition effect
+static void UpdateTransition(void);         
+// Draw transition effect (full-screen rectangle)
+static void DrawTransition(void);           
+// Update and draw one frame
+static void UpdateDrawFrame(void);         
 
-static void UpdateDrawFrame(void);          // Update and draw one frame
-
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
+/*The main function*/
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    /*This draws the game to be 1700 by 1000. Currently,
+    this is the resolution I want set for editing, as
+    full screen implementation seems like a tricky ass
+    thing to do*/
     const int screenWidth = 1700;
     const int screenHeight = 1000;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    /*This is our basic window, which we name so if we
+    have to */
+    InitWindow(screenWidth, screenHeight, 
+    "raylib [core] example - basic window");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60); /*This could be done where we have
+    FPS targetting based on system preference like in the 
+    example of the Steam Deck*/
     //--------------------------------------------------------------------------------------
 
     // Main game loop
