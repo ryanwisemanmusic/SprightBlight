94b56b49e1df4b417701588761aa9511bdd16def
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-09-30 16:58:32 -0700

    SprightBlight V0.02
    We have now multiple screens (with some problems, but text and formatting can be resized) that the user can cycle though. This is the fundamentals to some of the interact gestures I'll be using.
    
    I will be keeping the scope of this game relavtively small for now, just
    cause I want the ability to have a workable game, since working with raylib is not the easiest task

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -63,6 +63,11 @@ int main(void)
     have to */
     InitWindow(screenWidth, screenHeight, 
     "raylib [core] example - basic window");
+    GameScreen currentScreen = LOGO;
+
+    /*We initialize framesCounter to zero
+    for the sake of when we rerun said code*/
+    int framesCounter = 0;
 
     SetTargetFPS(60); /*This could be done where we have
     FPS targetting based on system preference like in the 
@@ -72,21 +77,105 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        switch (currentScreen)
+        {
+        case LOGO:
+        {
+        //TODO: Logic for screen here
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        framesCounter++;
+
+        /*After 120 frames of the logo animation,
+        go to title*/
+            if (framesCounter > 120)
+            {
+                currentScreen = TITLE;
+            } 
+        } break;
+        
+        /*Anything we want to put in our title
+        logic goes here. This would be a great
+        place for a Title Screen menu, which'll
+        be implemented eventually*/
+        case TITLE:
+            {
+                if (IsKeyPressed(KEY_ENTER) || 
+                IsGestureDetected(GESTURE_TAP))
+                {
+                    currentScreen = GAMEPLAY;
+                }
+            } break;
+
+        /*Any of our gameplay logic will go here, which is
+        gonna be a very large function in the end.*/
+        case GAMEPLAY:
+        {
+            if (IsKeyPressed(KEY_ENTER) || 
+            IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = ENDING;
+            }
+            
+        } break;
+
+        /*When we complete the game, this will
+        be any ending credit logic. This would
+        be great for if I were to have a huge
+        team working on the project. What we
+        will store here are any sound credits.*/
+        case ENDING:
+        {
+            if (IsKeyPressed(KEY_ENTER) ||
+            IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = TITLE;
+            }
+            
+        } break;
+        /*Since we will never be in default, we
+        just allow this sectio to break. I 
+        could be wrong about what goes here, though*/
+        default:
+            break;
+        }
+
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 400, 500, 40, LIGHTGRAY);
+        
+        /*We use a switch statement to cycle through different windows.
+        This will be a good chunk of the logic needed for the codebase.*/
+        switch (currentScreen)
+        {
+        case LOGO:
+            {
+                DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
+                DrawText("Wait for 2 seconds..." , 290, 220, 20, GRAY);
+            } break;
+        case TITLE:
+        {
+            DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+            DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+            DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN",
+            120, 220, 20, DARKGREEN);
+        } break;
+        case GAMEPLAY:
+        {
+            DrawRectangle(0, 0, screenWidth, screenHeight, PINK);
+            DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAGENTA);
+            DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN",
+            120, 220, 20, DARKGREEN);
+        } break;
+        case ENDING:
+        {
+            DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+            DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+            DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 
+            120, 220, 20, DARKBLUE);
+        }
+        default:
+            break;
+        }
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
