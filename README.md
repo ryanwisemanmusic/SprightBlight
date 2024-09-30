84354910b848376f18999222f16729012d4fbb14
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-09-30 13:34:55 -0700

    SprightBlight V0.01
    Basic project skeleton, only verified for MacOS.
    
    This game is a test game called SprightBlight

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# SprightBlight
- 
+This is a template for M1 Macs to run raylib with a basic window.
+
+I created this after the bulk majority of sources on the internet
+were terrible in getting raylib to compile on M1 Macs. 
+
+While I should clean this project up because it is a mess, this
+should easily run raylib on any M1/M2/M3/M(WhateverTheFuck) Mac
+
+I have decided to publish this for the sake of making people's 
+lives easier, as a working build of raylib just doesn't exist from
+scratch.
+
+In order to run this:
+1. cd into the src folder in terminal.
+2. regret your life decisions to use raylib over Godot/Unity/Unreal
+3. run the command "make" (remove the quotes)
+    this should compile all the files neccessary to run raylib. You
+    can choose to go back to the original .c file and implement the
+    standard template if you choose to. I'm not your mother, do
+    what yout want.
+4. run "./raylib_game"
---
+++ src/raylib_game.c
@@ -0,0 +1,96 @@
+/*******************************************************************************************
+*
+*   raylib game template
+*
+*   <Game title>
+*   <Game description>
+*
+*   This game has been created using raylib (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+//----------------------------------------------------------------------------------
+// Shared Variables Definition (global)
+// NOTE: Those variables are shared between modules through screens.h
+//----------------------------------------------------------------------------------
+GameScreen currentScreen = LOGO;
+Font font = { 0 };
+Music music = { 0 };
+Sound fxCoin = { 0 };
+
+//----------------------------------------------------------------------------------
+// Local Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+static const int screenWidth = 800;
+static const int screenHeight = 450;
+
+// Required variables to manage screen transitions (fade-in, fade-out)
+static float transAlpha = 0.0f;
+static bool onTransition = false;
+static bool transFadeOut = false;
+static int transFromScreen = -1;
+static GameScreen transToScreen = UNKNOWN;
+
+//----------------------------------------------------------------------------------
+// Local Functions Declaration
+//----------------------------------------------------------------------------------
+static void ChangeToScreen(int screen);     // Change to screen, no transition effect
+
+static void TransitionToScreen(int screen); // Request transition to next screen
+static void UpdateTransition(void);         // Update transition effect
+static void DrawTransition(void);           // Draw transition effect (full-screen rectangle)
+
+static void UpdateDrawFrame(void);          // Update and draw one frame
+
+//----------------------------------------------------------------------------------
+// Main entry point
+//----------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
